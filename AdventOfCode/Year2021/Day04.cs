365c54467cac7256c9393ff6a200f429ffb76cd3
kgroener <koengroener@hotmail.com>
2021-12-04 10:37:45 +0100

    Solved part 1

--- AdventOfCode/Year2021/Day04.cs
+++ AdventOfCode/Year2021/Day04.cs
@@ -16,43 +16,52 @@ Maybe it wants to play bingo?";
 
         public IEnumerable<IAdventDaySolution> GetSolutions()
         {
-            throw new NotImplementedException();
+            return new IAdventDaySolution[] { new Solution1(Boards, Numbers) };
         }
 
         internal class Solution1 : IAdventDaySolution
         {
+            private readonly int[][][] _boards;
+            private readonly int[] _numbers;
+
+            public Solution1(int[][][] boards, int[] numbers)
+            {
+                _boards = boards;
+                _numbers = numbers;
+            }
+
             public string Description => @"To guarantee victory against the giant squid, figure out which board will win first. What will your final score be if you choose that board?";
 
             public int Part => 1;
 
             public object Solve()
             {
-                var numberIndexes = Numbers.Select((v,i) => (Index: i, Value: v)).ToDictionary(kv => kv.Value, kv => kv.Index);
+                var numberIndexes = _numbers.Select((v,i) => (Index: i, Value: v)).ToDictionary(kv => kv.Value, kv => kv.Index);
 
-                var fastestBoard = Boards.Select((b, i) => (
+                var fastestBoard = _boards.Select((b, i) => (
                     BoardIndex: i,
                     Board: b,
                     NumberIndexForWin: Math.Min(
                         // Get first win for rows
                         b
-                        .Where(r => r.Intersect(Numbers).Count() == r.Length)
+                        .Where(r => r.Intersect(_numbers).Count() == r.Length)
                         .Min(r => r.Max(v => numberIndexes[v])),
                         // Get first win for columns
                         Enumerable
                         .Range(0, b.Length)
                         .Select(i => b.Select(r => r[i]).ToArray())
-                        .Where(c => c.Intersect(Numbers).Count() == c.Length)
+                        .Where(c => c.Intersect(_numbers).Count() == c.Length)
                         .Min(r => r.Max(v => numberIndexes[v])))
                     ))
                     .Aggregate((a,b) => a.NumberIndexForWin < b.NumberIndexForWin ? a: b);
 
-                return fastestBoard.Board.SelectMany(b => b).Except(Numbers.Take(fastestBoard.NumberIndexForWin)).Sum();
+                return fastestBoard.Board.SelectMany(b => b).Except(_numbers.Take(fastestBoard.NumberIndexForWin+1)).Sum() * _numbers.ElementAt(fastestBoard.NumberIndexForWin);
             }
         }
 
-        private static int[] Numbers = { 28, 82, 77, 88, 95, 55, 62, 21, 99, 14, 30, 9, 97, 92, 94, 3, 60, 22, 18, 86, 78, 71, 61, 43, 79, 33, 65, 81, 26, 49, 47, 51, 0, 89, 57, 75, 42, 35, 80, 1, 46, 83, 39, 53, 40, 36, 54, 70, 76, 38, 50, 23, 67, 2, 20, 87, 37, 66, 84, 24, 98, 4, 7, 12, 44, 10, 29, 5, 48, 59, 32, 41, 90, 17, 56, 85, 96, 93, 27, 74, 45, 25, 15, 6, 69, 16, 19, 8, 31, 13, 64, 63, 34, 73, 58, 91, 11, 68, 72, 52 };
+        private readonly int[] Numbers = { 28, 82, 77, 88, 95, 55, 62, 21, 99, 14, 30, 9, 97, 92, 94, 3, 60, 22, 18, 86, 78, 71, 61, 43, 79, 33, 65, 81, 26, 49, 47, 51, 0, 89, 57, 75, 42, 35, 80, 1, 46, 83, 39, 53, 40, 36, 54, 70, 76, 38, 50, 23, 67, 2, 20, 87, 37, 66, 84, 24, 98, 4, 7, 12, 44, 10, 29, 5, 48, 59, 32, 41, 90, 17, 56, 85, 96, 93, 27, 74, 45, 25, 15, 6, 69, 16, 19, 8, 31, 13, 64, 63, 34, 73, 58, 91, 11, 68, 72, 52 };
 
-        private static int[][][] Boards =
+        private readonly int[][][] Boards =
         {
             new int[][] {
                 new int[] { 31, 88, 71, 23, 61 },
