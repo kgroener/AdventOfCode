6e3a6999428ae3973207d4cf2c81707f0143ef3e
Koen Groener <k.groener@bronkhorst.com>
2021-12-07 08:12:14 +0100

    Solved day 7

---
+++ AdventOfCode.Tests/Year2021/Day07Tests.cs
@@ -0,0 +1,46 @@
+using AdventOfCode.Year2021;
+using NUnit.Framework;
+
+namespace AdventOfCode.Tests.Year2021
+{
+    public class Day07Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Year2021_Day7_Part1_ExampleIsCorrect()
+        {
+            var solution = new Day07.Solution1(new []{ 16, 1, 2, 0, 4, 2, 7, 1, 2, 14 });
+
+            Assert.AreEqual(37, solution.Solve());
+        }
+
+
+        [Test]
+        public void Year2021_Day7_Part1_PuzzleIsCorrect()
+        {
+            var solution = new Day07.Solution1(Day07.Positions);
+
+            Assert.AreEqual(329389, solution.Solve());
+        }
+
+        [Test]
+        public void Year2021_Day7_Part2_ExampleIsCorrect()
+        {
+            var solution = new Day07.Solution2(new[] { 16, 1, 2, 0, 4, 2, 7, 1, 2, 14 });
+
+            Assert.AreEqual(168, solution.Solve());
+        }
+
+        [Test]
+        public void Year2021_Day7_Part2_PuzzleIsCorrect()
+        {
+            var solution = new Day07.Solution2(Day07.Positions);
+
+            Assert.AreEqual(168, solution.Solve());
+        }
+    }
+}
